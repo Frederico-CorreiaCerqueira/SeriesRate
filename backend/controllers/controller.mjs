3c3136be97656a4fbc23c4a4d1a1c4b562a6ea5b
Frederico-CorreiaCerqueira <fredericoccerqueira@gmail.com>
2024-08-24 15:56:49 +0100

    DataBase Schema Diagram

--- backend/controllers/controller.mjs
+++ backend/controllers/controller.mjs
@@ -3,10 +3,16 @@ import express from 'express';
 const app = express()
 const PORT = 3000;
 
+app.use(express.json());
 const user = express.Router();
 
 user.get('/', (req, res) => {
     res.send('GET request to the userpage')
+})
+
+user.post('/createUser',(req,res) =>{
+    console.log(req.body)
+    res.send('User created')
 })
 
 app.use('/user', user);
